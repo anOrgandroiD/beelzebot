91e4583eec596f2ad6382ee325934045b5e4f8f6
anOrgandroiD <anorgandroid@outlook.com>
2022-01-12 10:45:49 -0500

    admins can delete any role that isn't theirs, but it drains their soul a little

---
+++ commands/harmful/deleterole.js
@@ -0,0 +1,37 @@
+const Discord = require('discord.js')
+
+module.exports = {
+    name: "deleterole",
+    administrator: false,
+
+    async execute(client, message, args) {
+        const SOUL_DRAIN_AMNT = 0.25
+
+        userID = message.author.id
+        roleName = args[0]
+        modifiedRoleName = args[0].replace(/_/g, ' ')
+        let role = message.guild.roles.cache.find(x => x.name === modifiedRoleName)
+
+        if(role === undefined) {
+            await message.reply("NO ROLE OF THAT NAME EXISTS DINGUS")
+            return
+        }
+
+        let child = await client.data.getUserDB(message.author.id, message.author.tag)
+
+        if(child.roles.find(x => x.name === roleName) === undefined && !message.member.permissionsIn(message.channel).has('ADMINISTRATOR')) {
+            await message.reply("YOU CAN'T DELETE A ROLE YOU DIDN'T CREATE")
+            return
+        }
+
+        if(child.roles.find(x => x.name === roleName) === undefined) {
+            await client.data.soulDrain(userID, SOUL_DRAIN_AMNT)
+            await message.reply("ROLE SUCCESSFULLY BANISHED... FOR A PRICE OF COURSE")
+        } else {
+            await message.reply("ROLE SUCCESSFULLY BANISHED")
+        }
+
+        role.delete()
+        await client.data.deleteCustomRole(userID, roleName)
+    }
+}
